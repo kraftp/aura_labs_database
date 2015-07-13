18017f02313732050ab82ab161688573665b1617
kraftp <pkraft@college.harvard.edu>
2015-07-13 14:34:58 -0700

    Added csv-import

--- aura_labs_database/settings.py
+++ aura_labs_database/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = (
+    'csvimport.app.CSVImportConf',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
